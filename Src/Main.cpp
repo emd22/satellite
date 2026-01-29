92928de276917afcf8937be2091e5e04cf2479e1
Ethan MacDonald <e@ethanm.ca>
2026-01-28 22:40:36 -0400

    Added viewing position of satellite as sphere around Earth

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -8,9 +8,92 @@
 
 #include "SGP4.h"
 #include "raylib.h"
+#include "rlgl.h"
+
+
+class Vec3r
+{
+public:
+    static const Vec3r sZero;
+
+public:
+    Vec3r(double x, double y, double z) : X(x), Y(y), Z(z) {}
+
+    void Print() { printf("{ %f, %f, %f }\n", X, Y, Z); }
+
+    Vec3r operator*(float multiplier) const { return Vec3r(X * multiplier, Y * multiplier, Z * multiplier); }
+
+public:
+    union
+    {
+        struct
+        {
+            double X;
+            double Y;
+            double Z;
+            double W;
+        };
+
+        double Values[4];
+    };
+};
+
+const Vec3r Vec3r::sZero = Vec3r(0.0, 0.0, 0.0);
+
+struct SpaceForce
+{
+    Vec3r Position = Vec3r::sZero;
+    Vec3r Velocity = Vec3r::sZero;
+};
+
+
+class Space
+{
+public:
+    void CreateFromEntry(char* line1, char* line2);
+
+    SpaceForce GetForce(double time);
+
+
+public:
+    struct
+    {
+        double Start;
+        double Stop;
+        double Delta;
+    } Mfe;
+
+    elsetrec SatRec;
+};
+
+void Space::CreateFromEntry(char* line1, char* line2)
+{
+    memset(&SatRec, 0, sizeof(SatRec));
+    SGP4Funcs::twoline2rv(line1, line2, 'c', 'e', 'i', wgs84, Mfe.Start, Mfe.Stop, Mfe.Delta, SatRec);
+}
+
+SpaceForce Space::GetForce(double time)
+{
+    SpaceForce force {};
+    SGP4Funcs::sgp4(SatRec, time, force.Position.Values, force.Velocity.Values);
+
+    return force;
+}
+
 
 int main()
 {
+    char line1[] = "1 41340U 16012D   26026.21998329  .00083066  00000+0  11410-2 0  9992";
+    char line2[] = "2 41340  30.9947   3.4457 0004047 308.3989  51.6299 15.55929722548471 ";
+
+    Space space {};
+    space.CreateFromEntry(strdup(line1), strdup(line2));
+
+    SpaceForce force = space.GetForce(0.0f);
+
+    force.Position.Print();
+    force.Velocity.Print();
+
     InitWindow(1024, 720, "Space");
 
     SetTargetFPS(60);
@@ -24,6 +107,14 @@ int main()
 
     Model model = LoadModel("../earth.glb");
 
+    Vec3r sat_pos_scaled = force.Position * 0.1;
+    sat_pos_scaled.Print();
+
+    Vector3 sat_position = Vector3 { .x = float(sat_pos_scaled.X),
+                                     .y = float(sat_pos_scaled.Y),
+                                     .z = float(sat_pos_scaled.Z) };
+
+    double counter = 0.0f;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -32,10 +123,21 @@ int main()
 
         DrawModel(model, Vector3 { 0.0f, 0.0f, 0.0f }, 0.05f, WHITE);
 
+        camera.position.x = cosf(counter * 0.1) * 70.0f;
+        camera.position.z = sinf(counter * 0.1) * 70.0f;
+
+        DrawSphere(sat_position, 5.0f, RED);
+
         EndMode3D();
 
+        // rlPushMatrix();
+
+
+        // rlPopMatrix();
 
         EndDrawing();
+
+        counter += 0.1f;
     }
 
     UnloadModel(model);
@@ -44,89 +146,6 @@ int main()
 
 
     return 0;
+
+    return 0;
 }
-
-
-// class Vec3r
-// {
-// public:
-//     static const Vec3r sZero;
-
-// public:
-//     Vec3r(double x, double y, double z) : X(x), Y(y), Z(z) {}
-
-//     void Print() { printf("{ %f, %f, %f }\n", X, Y, Z); }
-
-// public:
-//     union
-//     {
-//         struct
-//         {
-//             double X;
-//             double Y;
-//             double Z;
-//             double W;
-//         };
-
-//         double Values[4];
-//     };
-// };
-
-// const Vec3r Vec3r::sZero = Vec3r(0.0, 0.0, 0.0);
-
-// struct SpaceForce
-// {
-//     Vec3r Position = Vec3r::sZero;
-//     Vec3r Velocity = Vec3r::sZero;
-// };
-
-
-// class Space
-// {
-// public:
-//     void CreateFromEntry(char* line1, char* line2);
-
-//     SpaceForce GetForce(double time);
-
-
-// public:
-//     struct
-//     {
-//         double Start;
-//         double Stop;
-//         double Delta;
-//     } Mfe;
-
-//     elsetrec SatRec;
-// };
-
-// void Space::CreateFromEntry(char* line1, char* line2)
-// {
-//     memset(&SatRec, 0, sizeof(SatRec));
-//     SGP4Funcs::twoline2rv(line1, line2, 'c', 'e', 'i', wgs84, Mfe.Start, Mfe.Stop, Mfe.Delta, SatRec);
-// }
-
-// SpaceForce Space::GetForce(double time)
-// {
-//     SpaceForce force {};
-//     SGP4Funcs::sgp4(SatRec, time, force.Position.Values, force.Velocity.Values);
-
-//     return force;
-// }
-
-
-// int main()
-// {
-//     char line1[] = "1 41340U 16012D   26026.21998329  .00083066  00000+0  11410-2 0  9992";
-//     char line2[] = "2 41340  30.9947   3.4457 0004047 308.3989  51.6299 15.55929722548471 ";
-
-//     Space space {};
-//     space.CreateFromEntry(strdup(line1), strdup(line2));
-
-//     SpaceForce force = space.GetForce(0.0f);
-
-//     force.Position.Print();
-//     force.Velocity.Print();
-
-//     return 0;
-// }
